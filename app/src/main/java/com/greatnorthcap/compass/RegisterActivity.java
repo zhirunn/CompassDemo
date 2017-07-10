8f8713071621607cf9bb35c8e4b29fdfe05d0322
zhirunn <zhirunn@gmail.com>
2017-07-10 14:56:52 -0600

    simple page trasversal

---
+++ app/src/main/java/com/greatnorthcap/compass/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.greatnorthcap.compass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by aspiree15 on 10/07/17.
+ */
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
