896aac8dc2a42df19aae8cd389dd2e06fbb0b393
Dan Laganson <dlaganson1@studentmail.nait.ca>
2017-07-17 11:34:52 -0700

    fixed

--- php_files/information.php
+++ php_files/information.php
@@ -6,13 +6,14 @@
         $address = $_POST['Address'];
         $employment = $_POST['Employment'];
         $jobtitle = $_POST['JobTitle'];
-        $phodocuments = $_POST['PhotoDocuments'];
+        $photodocuments = $_POST['PhotoDocuments'];
 		require_once "connect.php";
-		$idsql = "SELECT * FROM Users WHERE Email = '$email'";
 
+		$idsql = "SELECT * FROM Users WHERE Email = '$email'";
 		$row = mysqli_fetch_row(mysqli_query($conn, $idsql));
 		$userid = $row[0];
-		$sql = "INSERT INTO AccountDetails (UserID, FullName,PhoneNumber,Address,Employment,JobTitle,PhotoDocuments) VALUES ('$userid','$fullname','$phonenumber','$address','employment','jobtitle','photodocuments')";
+
+		$sql = "INSERT INTO AccountDetails (UserID, FullName,PhoneNumber,Address,Employment,JobTitle,PhotoDocuments) VALUES ('$userid','$fullname','$phonenumber','$address','$employment','$jobtitle','$photodocuments')";
 
 		if(mysqli_query($conn, $sql)) {
 			ob_clean();
