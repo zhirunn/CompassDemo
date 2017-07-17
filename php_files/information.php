3d4a6b39e042e539f693c7c1a7b6d3281b0f2a11
Dan Laganson <dlaganson1@studentmail.nait.ca>
2017-07-17 11:33:01 -0700

    information php

---
+++ php_files/information.php
@@ -0,0 +1,27 @@
+<?php
+	if($_SERVER['REQUEST_METHOD']=='POST') {
+		$email = $_POST['Email'];
+		$fullname = $_POST['FullName'];
+        $phonenumber = $_POST['PhoneNumber'];
+        $address = $_POST['Address'];
+        $employment = $_POST['Employment'];
+        $jobtitle = $_POST['JobTitle'];
+        $phodocuments = $_POST['PhotoDocuments'];
+		require_once "connect.php";
+		$idsql = "SELECT * FROM Users WHERE Email = '$email'";
+
+		$row = mysqli_fetch_row(mysqli_query($conn, $idsql));
+		$userid = $row[0];
+		$sql = "INSERT INTO AccountDetails (UserID, FullName,PhoneNumber,Address,Employment,JobTitle,PhotoDocuments) VALUES ('$userid','$fullname','$phonenumber','$address','employment','jobtitle','photodocuments')";
+
+		if(mysqli_query($conn, $sql)) {
+			ob_clean();
+			echo "Successfully Uploaded";
+		} else {
+			ob_clean();
+			echo "Could not upload information";
+		}
+	} else {
+		echo "Unexpected Error.";
+	}
+?>
