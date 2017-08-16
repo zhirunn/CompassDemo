d4d0da9262b2024dbca95902dba0a5af1158e3d3
Dan Laganson <dlaganson1@studentmail.nait.ca>
2017-08-16 02:14:59 -0600

    android manifest

---
+++ php_files/usercheck.php
@@ -0,0 +1,14 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST') {
+require_once "connect.php";
+$id = $_POST["UserID"];
+$sql = "SELECT BorrowerType FROM Users WHERE UserID = $id";
+$res = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($res);
+$admincheck = $row["LenderType"];
+ob_clean();
+echo $admincheck;
+mysqli_close($conn);
+}
+
+?>
