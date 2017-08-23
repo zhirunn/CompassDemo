5820afc3b5ef5dbe1e0d38d759591cced7f0b569
zhirunn <zhirunn@gmail.com>
2017-08-23 16:21:09 -0600

    added a check to user's personal information(text). Still need to add a check for images.

--- app/src/main/java/com/greatnorthcap/compass/AccountProfileActivity.java
+++ app/src/main/java/com/greatnorthcap/compass/AccountProfileActivity.java
@@ -81,8 +81,7 @@ public class AccountProfileActivity extends AppCompatActivity {
             Map<String, String> params = new HashMap<>();
             params.put(UserPref.getKeyUserId(), ID);
             return params;
-        }}
-                ;
+        }};
         requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringGetRequest);
 
