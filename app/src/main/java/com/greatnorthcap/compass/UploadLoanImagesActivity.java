c0bdb2796c7ba65efd7089e3effc5230a90a3b4b
zhirunn <zhirunn@gmail.com>
2017-08-25 18:04:38 -0600

    okay, checking the image url works now

--- app/src/main/java/com/greatnorthcap/compass/UploadLoanImagesActivity.java
+++ app/src/main/java/com/greatnorthcap/compass/UploadLoanImagesActivity.java
@@ -4,9 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,11 +12,6 @@ import android.widget.Toast;
 
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by aspiree15 on 02/08/17.
@@ -62,37 +55,119 @@ public class UploadLoanImagesActivity extends Activity {
         String PreAuthorizedAgreementURL = UserPref.getServerAddress() + "Images/" + loanid + "/PreAuthorizedAgreement.jpg";
 
         try {
-            Picasso.with(this).load(BankStatementURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBNinetyDayBankStatement);
-            Picasso.with(this).load(FirstPayStubURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBFirstPayStub);
-            Picasso.with(this).load(SecondPayStubURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBSecondPayStub);
-            Picasso.with(this).load(ThirdPayStubURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBThirdPayStub);
-            Picasso.with(this).load(DriversIDURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBGovID);
-            Picasso.with(this).load(EmploymentLetterURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBEmploymentLetter);
-            Picasso.with(this).load(ProofOfAddressURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBAddressProof);
-            Picasso.with(this).load(PreAuthorizedDebitURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBPreAuthorizedDebit);
-            Picasso.with(this).load(SocialInsuranceNumberURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBSIN);
-            Picasso.with(this).load(OtherIDURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBAnotherID);
-            Picasso.with(this).load(PreAuthorizedAgreementURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBPreAuthorizedAgreement);
+            Picasso.with(this).load(BankStatementURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBNinetyDayBankStatement, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    BankStatementURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    BankStatementURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(FirstPayStubURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBFirstPayStub, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    FirstPayStubURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    FirstPayStubURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(SecondPayStubURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBSecondPayStub, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    SecondPayStubURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    SecondPayStubURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(ThirdPayStubURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBThirdPayStub, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    ThirdPayStubURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    ThirdPayStubURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(DriversIDURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBGovID, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    DriversIDURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    DriversIDURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(EmploymentLetterURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBEmploymentLetter, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    EmploymentLetterURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    EmploymentLetterURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(ProofOfAddressURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBAddressProof, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    ProofOfAddressURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    ProofOfAddressURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(PreAuthorizedDebitURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBPreAuthorizedDebit, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    PreAuthorizedDebitURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    PreAuthorizedDebitURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(SocialInsuranceNumberURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBSIN, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    SocialInsuranceNumberURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    SocialInsuranceNumberURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(OtherIDURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBAnotherID, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    OtherIDURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    OtherIDURLCheck = false;
+                }
+            });
+            Picasso.with(this).load(PreAuthorizedAgreementURL).networkPolicy(NetworkPolicy.NO_CACHE).into(imageBPreAuthorizedAgreement, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    PreAuthorizedAgreementURLCheck = true;
+                }
+                @Override
+                public void onError() {
+                    PreAuthorizedAgreementURLCheck = false;
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        /*
-        BankStatementURLCheck = Patterns.WEB_URL.matcher(BankStatementURL).matches();
-        FirstPayStubURLCheck = Patterns.WEB_URL.matcher(FirstPayStubURL).matches();
-        SecondPayStubURLCheck = Patterns.WEB_URL.matcher(SecondPayStubURL).matches();
-        ThirdPayStubURLCheck = Patterns.WEB_URL.matcher(ThirdPayStubURL).matches();
-        DriversIDURLCheck = Patterns.WEB_URL.matcher(DriversIDURL).matches();
-        EmploymentLetterURLCheck = Patterns.WEB_URL.matcher(EmploymentLetterURL).matches();
-        ProofOfAddressURLCheck = Patterns.WEB_URL.matcher(ProofOfAddressURL).matches();
-        PreAuthorizedDebitURLCheck = Patterns.WEB_URL.matcher(PreAuthorizedDebitURL).matches();
-        SocialInsuranceNumberURLCheck = Patterns.WEB_URL.matcher(SocialInsuranceNumberURL).matches();
-        OtherIDURLCheck = Patterns.WEB_URL.matcher(OtherIDURL).matches();
-        PreAuthorizedAgreementURLCheck = Patterns.WEB_URL.matcher(PreAuthorizedAgreementURL).matches();
-        */
-
-        //BankStatementURLCheck = CheckURL(BankStatementURL);
-
 
         imageBNinetyDayBankStatement.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -207,10 +282,9 @@ public class UploadLoanImagesActivity extends Activity {
         buttonApplyForLoan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!BankStatementURLCheck) {
-                /*if (!BankStatementURLCheck || !FirstPayStubURLCheck || !SecondPayStubURLCheck || !ThirdPayStubURLCheck || !DriversIDURLCheck
+                if (!BankStatementURLCheck || !FirstPayStubURLCheck || !SecondPayStubURLCheck || !ThirdPayStubURLCheck || !DriversIDURLCheck
                         || !EmploymentLetterURLCheck || !ProofOfAddressURLCheck || !PreAuthorizedDebitURLCheck || !SocialInsuranceNumberURLCheck
-                        || !OtherIDURLCheck || !PreAuthorizedAgreementURLCheck) {*/
+                        || !OtherIDURLCheck || !PreAuthorizedAgreementURLCheck) {
                     Toast.makeText(UploadLoanImagesActivity.this, "Please upload all of the required relevant documents.", Toast.LENGTH_LONG).show();
                 } else {
                     //This toast needs to be changed.
@@ -219,27 +293,4 @@ public class UploadLoanImagesActivity extends Activity {
             }
         });
     }
-
-    /*
-    private Boolean CheckURL(String url) {
-        final ScheduledThreadPoolExecutor myTimer = new ScheduledThreadPoolExecutor(1);
-        myTimer.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                Boolean result;
-                try {
-                    HttpURLConnection.setFollowRedirects(false);
-                    HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-                    conn.setRequestMethod("HEAD");
-                    result = (conn.getResponseCode() == HttpURLConnection.HTTP_OK);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    result = false;
-                }
-            }
-        }, 0,10000, TimeUnit.MILLISECONDS);
-        return result;
-    }
-    */
-
 }
